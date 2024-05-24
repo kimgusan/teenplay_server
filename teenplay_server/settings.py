68a4336ac0c630a18edcb854d6f868caba9b59e3
kimkyusan <kimkyusan94@gmail.com>
2024-05-24 09:24:15 +0900

    setting update

--- teenplay_server/settings.py
+++ teenplay_server/settings.py
@@ -78,8 +78,8 @@ CRONJOBS = [
     ('0 0 * * *', 'activity.tasks.DeleteOldImages')
 ]
 
-SITE_ID = 2
-# SITE_ID = 17
+# SITE_ID = 2
+SITE_ID = 17
 
 SOCIALACCOUNT_AUTO_SIGNUP = True
 ACCOUNT_EMAIL_REQUIRED = True
@@ -146,8 +146,8 @@ DATABASES = {
         # 계정 비밀번호
         'PASSWORD': '1234',
         # DBMS가 설치된 서버 PC의 IP, (127.0.0.1 = 로컬)
-        # 'HOST': '52.78.208.126',
-        'HOST': '13.125.213.142', # 현이 서버
+        'HOST': '52.78.208.126',
+        # 'HOST': '13.125.213.142', # 현이 서버
         # DBMS의 포트번호
         'PORT': '3306'
     }
